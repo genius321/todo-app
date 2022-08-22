60ff87252b59f217b44885bf0a88e7a8508d4c78
Konstantin Orlov <genius4666@gmail.com>
2022-08-22 03:46:25 +0300

    added configuration files and viper initialization

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/genius321/todo-app/pkg/handler"
 	"github.com/genius321/todo-app/pkg/repository"
 	"github.com/genius321/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializining configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRouts()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRouts()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
