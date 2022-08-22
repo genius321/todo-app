1e97adbdf5f3a088637468423765c2baa1db79fd
Konstantin Orlov <genius4666@gmail.com>
2022-08-23 00:37:35 +0300

    added postgres db initialization

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/genius321/todo-app/pkg/handler"
 	"github.com/genius321/todo-app/pkg/repository"
 	"github.com/genius321/todo-app/pkg/service"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -15,7 +16,19 @@ func main() {
 		log.Fatalf("error initializining configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("failed to initialized db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
